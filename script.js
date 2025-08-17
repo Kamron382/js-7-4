4fa2051b28c68fad9cc328fda771cd9f65e54344
Kamron <k1367259@gmail.com>
2025-08-17 15:37:18 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let h1 = document.querySelector('h1');
+let p = document.querySelector('p');
+new Typed(h1, {
+  strings: ['TEXT GRADIENT'],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+new Typed(p, {
+  strings: ['Make your headlines stand out'],
+    typeSpeed: 40,
+    backSpeed: 25,
+    loop: true
+});
